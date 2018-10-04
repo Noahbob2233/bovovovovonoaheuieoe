e9ec32354a05241cd9b60b5ecef45dd077a85d01
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-10-04 11:22:17 -0600

    Bring in material-ui

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -11,14 +12,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <Button variant="contained" color="primary"
             href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </Button>
         </header>
       </div>
     );
